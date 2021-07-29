6da367305626efe3b3446ffefbbf5e12f390576a
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-07-29 16:49:21 -0300

    mimeTypes validation

--- backend/src/services/upload.ts
+++ backend/src/services/upload.ts
@@ -2,11 +2,13 @@ import multer from 'multer';
 import path from 'path';
 import { v4 } from 'uuid';
 
+const mimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
 const storage = multer.diskStorage({
-    destination(req, file, cb) {
+    destination (req, file, cb) {
         cb(null, path.resolve(__dirname, '..', '..', 'uploads'));
     },
-    filename(req, file, cb) {
+    filename (req, file, cb) {
         cb(null, `${v4()}-${file.originalname}`);
     }
 });
@@ -15,6 +17,12 @@ const upload = multer({
     storage,
     limits: {
         fileSize: 6e+6
+    },
+    fileFilter (req, file, cb) {
+        if (mimeTypes.includes(file.mimetype)) {
+            cb(null, true);
+        }
+        cb(null, false);
     }
 });
 
