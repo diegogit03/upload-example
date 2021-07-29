0ba389fcde4adf5eef6f2c0b122569a41ddaeccc
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-07-28 21:58:46 -0300

    setup mongodb connection

--- backend/src/start/app.ts
+++ backend/src/start/app.ts
@@ -1,5 +1,13 @@
-import express from 'express';
-import routes from './routes';
+import path     from 'path';
+import * as dotenv   from 'dotenv'
+import express  from 'express';
+import mongoose from 'mongoose';
+
+import routes   from './routes';
+
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') })
+
+mongoose.connect(process.env.MONGO_URI ?? '', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
